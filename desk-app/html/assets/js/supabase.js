616814a934e87a722d5a832d3747ec5bf6e90e6e
Laura <153616329+Larxkuf@users.noreply.github.com>
2025-05-24 19:49:57 +0200

    Delete desk-app directory

--- desk-app/html/assets/js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://hqzzeamhqjhemgpacktz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxenplYW1ocWpoZW1ncGFja3R6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyODQ5ODgsImV4cCI6MjA1Nzg2MDk4OH0.fYEhXioGCH8gR8d2osDXT0gNpovn-qBmvzoqgNaaOo8'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
